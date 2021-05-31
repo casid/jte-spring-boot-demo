7fce9893669eac20d088db6a5bc7964eff1e04d9
Benjamin <benjamin.guy.thomas@gmail.com>
2021-05-31 20:37:27 +0200

    Add dev/prod config

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 ## jte Spring Boot demo
 
 A simple demo of jte running with Spring Boot.
+
+---
+
+### Deploy to Heroku
+
+Requirements: Heroku CLI
+
+- git clone
+- cd into project
+- heroku apps:create jte-demo --region eu
+  - echo "This creates a 'heroku' remote repo"
+  - git remote -v show
+- git push heroku
+- https://jte-demo.herokuapp.com/
+- heroku apps:destroy jte-demo
+
--- src/main/java/gg/jte/demo/DemoController.java
+++ src/main/java/gg/jte/demo/DemoController.java
@@ -1,8 +1,10 @@
 package gg.jte.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -13,6 +15,15 @@ public class DemoController {
     TemplateRenderer templateRenderer;
     @Autowired
     VisitsRepository visitsRepository;
+
+    @Value("${spring.profiles.active:PROFILE_NOT_SET}")
+    private String profile;
+
+    @ResponseBody
+    @GetMapping(value = "/profile")
+    public String profile() {
+        return profile;
+    }
 
     @GetMapping("/")
     public void view(HttpServletResponse response) {
